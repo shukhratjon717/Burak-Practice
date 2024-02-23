324a56a225fe0edf82815361c69d29e17e1deebe
Shukhratjon <shukhratjon0717@gmail.com>
2024-02-23 18:13:32 +0900

    feat: Initial Setup

---
+++ scr/server.ts
@@ -0,0 +1,9 @@
+console.log("executed")
+
+import moment from "moment";
+const currentTime = moment().format("YYYY-MM-DD")
+console.log(currentTime, "time");
+
+
+const peson:string = "Steve";
+const count:number = 100;

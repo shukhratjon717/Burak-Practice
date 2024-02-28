2fbad1d6cba7d2a1779391fdd7c0cb7fa1d81487
Shukhratjon <shukhratjon0717@gmail.com>
2024-02-28 14:41:21 +0900

    feat: create routing member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router"
 
 /**  1 - ENTERANCE**/
 const app = express();
@@ -15,5 +16,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /**  4 - ROUTERS**/
+app.use("/", router)            // Middleware disgn pattern
 
 export default app;     // module.exports = app

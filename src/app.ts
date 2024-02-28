d1d39bf3f06d3c24f6c3c02aea8073e55f687271
Shukhratjon <shukhratjon0717@gmail.com>
2024-02-28 15:01:35 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import path from "path";
-import router from "./router"
+import router from "./router";
+import routerAdmin from "./routerAdmin"
 
 /**  1 - ENTERANCE**/
 const app = express();
-// console.log("__dirname", __dirname)
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
  
 
 /**  2 - SESSIONS**/
@@ -16,6 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /**  4 - ROUTERS**/
-app.use("/", router)            // Middleware disgn pattern
+app.use("/admin", routerAdmin);  // EJS
+app.use("/", router)             // REACT
 
 export default app;     // module.exports = app
--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,30 +2,7 @@ import {Request, Response} from "express";
 
 import {T} from "../libs/types/common";
 
+// REACT
+
 const memberController: T = {};
-
-memberController.goHome = (req: Request, res: Response) => {
-    try {
-        res.send("Home Page")
-    } catch (err) {
-        console.log("Error, goHome:", err)
-    }
-};
-
-memberController.getLogin = (req: Request, res: Response) => {
-    try {
-        res.send("Login Page")
-    } catch (err) {
-        console.log("Error, getLogin:", err)
-    }
-};
-
-memberController.getSignup = (req: Request, res: Response) => {
-    try {
-        res.send("Signup Page")
-    } catch (err) {
-        console.log("Error, getSignup:", err)
-    }
-};
-
 export default memberController;

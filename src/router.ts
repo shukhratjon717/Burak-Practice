4d3fe608e2b2641e435ee8aa1ff6df04da257450
Shukhratjon <shukhratjon0717@gmail.com>
2024-02-28 16:03:10 +0900

    feat: member servicce model integration

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,4 @@
 import express from "express";
 const router = express.Router();
-import memberController from "./controllers/member.controller"; 
-
+import memberController from "./controllers/member.controller";  
 export default router;

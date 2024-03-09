bb2a1742dad001d65f64ec15630aba20e0abef82
Shukhratjon <shukhratjon0717@gmail.com>
2024-03-09 18:51:03 +0900

    feat: Task-J completion

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 interface findbiggest {
   str: string;
 
@@ -18,6 +19,23 @@ function findLongest(str: string): findbiggest{
   };
   
   console.log(findLongest("Hello world by Steven"))
+=======
+interface GetDigits {
+  str: string;
+  nums: string;
+}
+
+function findNums(str: string): GetDigits{
+  const step1 = str.match(/\d/g);
+  const step2 = step1 ? step1.join("") : "";
+  return {str: "final output", nums: step2}
+}
+
+const result = findNums("hello3323wo7r1l7d")
+console.log(result.str, result.nums)
+
+
+>>>>>>> parent of 670e6ad (feat: create train area)
 
 /* 
 Traditional FrontEnd Development (FD) => BSSR = SSR (Admin) => EJS

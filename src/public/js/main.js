039afef6bdb5690097fd6cf5567c81c7efec7e47
Shukhratjon <shukhratjon0717@gmail.com>
2024-03-09 18:37:13 +0900

    Task-J: Compleetion

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,2 @@
 console.log("FRONTEND JAVASCRIPT STARTS");
+
--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,23 @@
-interface GetDigits {
+interface findbiggest {
   str: string;
-  nums: string;
+
 }
 
-function findNums(str: string): GetDigits{
-  const step1 = str.match(/\d/g);
-  const step2 = step1 ? step1.join("") : "";
-  return {str: "final output", nums: step2}
-}
-
-const result = findNums("hello3323wo7r1l7d")
-console.log(result.str, result.nums)
-
-
+function findLongest(str: string): findbiggest{
+  
+    let step1 = str.split(" ");
+    let longest = 0;
+    let result = '';
+    for(let i = 0; i < step1.length; i++) {
+      if(longest < step1[i].length) {
+        longest = step1[i].length;
+        result = step1[i]
+      }
+    }
+    return {"str": result}
+  };
+  
+  console.log(findLongest("Hello world by Steven"))
 
 /* 
 Traditional FrontEnd Development (FD) => BSSR = SSR (Admin) => EJS

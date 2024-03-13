a86a431306780d1d95a68e527fda4a1c7484391a
Shukhratjon <shukhratjon717@gmail.com>
2024-03-13 15:42:49 +0900

    feat: upload files to server via multer

--- src/libs/Error.ts
+++ src/libs/Error.ts
@@ -1,40 +1,39 @@
 export enum HttpCode {
-    OK = 200,
-    CREATED = 304,
-    BAD_REQUEST = 400,
-    UNAUTHORIZED = 401,
-    FORBIDDEN = 403,
-    NOT_FOUND = 404,
-    INTERNAL_SERVER_ERROR = 500,
+  OK = 200,
+  CREATED = 304,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
 }
 
-
 export enum Message {
-    SOEMTHING_WENT_WRONG = " Something went wrong!",
-    NO_DATA_FOUND = "No data is found!",
-    CREATE_FAILED = "Create is failed!",
-    UPDATE_FAILED = "Update is failed!",
+  SOEMTHING_WENT_WRONG = " Something went wrong!",
+  NO_DATA_FOUND = "No data is found!",
+  CREATE_FAILED = "Create is failed!",
+  UPDATE_FAILED = "Update is failed!",
 
-    USED_NICK_PHONE = "You are inserting already used phone or nick!",
-    NO_MEMBER_NICK = "No member with this member nickname!",
-    WRONG_PASSWORD = "Wrong password inserted!, please try again!",
-    NOT_AUTHENTICATED = "You are not authenticated, Please login first!"
-};
+  USED_NICK_PHONE = "You are inserting already used phone or nick!",
+  NO_MEMBER_NICK = "No member with this member nickname!",
+  WRONG_PASSWORD = "Wrong password inserted!, please try again!",
+  NOT_AUTHENTICATED = "You are not authenticated, Please login first!",
+}
 
 class Errors extends Error {
-    public code: HttpCode;
-    public message: Message;
+  public code: HttpCode;
+  public message: Message;
 
-    static standard = {
-        code: HttpCode.INTERNAL_SERVER_ERROR,
-        message: Message.SOEMTHING_WENT_WRONG,
-    };
+  static standard = {
+    code: HttpCode.INTERNAL_SERVER_ERROR,
+    message: Message.SOEMTHING_WENT_WRONG,
+  };
 
-    constructor(statusCode: HttpCode, statusMessage: Message) {
-        super();
-        this.code = statusCode;
-        this.message = statusMessage;
-    }
+  constructor(statusCode: HttpCode, statusMessage: Message) {
+    super();
+    this.code = statusCode;
+    this.message = statusMessage;
+  }
 }
 
 export default Errors;
